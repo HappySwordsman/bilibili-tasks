e43cd32c218c02df0f614fba2db7e667c5ce4561
newblue <greecenew@foxmail.com>
2021-12-13 09:29:26 +0800

    fix: 日期显示问题

--- utils/notice.js
+++ utils/notice.js
@@ -6,8 +6,8 @@ const ding = new RobotDing(config.ding);
 module.exports = (content) => {
     ding.sendDing(
         {
-            title: `${dayjs().format("YYYY-MM-DD HH:mm:ss")} Bilibili 通知：`,
-            text: `### ${dayjs().format("YYYY-MM-DD HH:mm:ss")} Bilibili 通知： \n ${content}`
+            title: `${dayjs(new Date()).format("YYYY-MM-DD HH:mm:ss")} Bilibili 通知：`,
+            text: `### ${dayjs(new Date()).format("YYYY-MM-DD HH:mm:ss")} Bilibili 通知： \n ${content}`
         },
         "markdown"
     );
